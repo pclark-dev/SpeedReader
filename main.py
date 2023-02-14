31f4e73e1d0134a2dbb7b86972a4d9af58aedc42
Peyton <124329926+pclark-dev@users.noreply.github.com>
2023-02-14 16:16:18 -0600

    Add processing function (needs revision)

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ class main(customtkinter.CTk):
         self.radiobutton_frame.grid_columnconfigure((0,1), weight=1)
         self.radiobutton_frame.grid_rowconfigure((0,1), weight=1)
 
-        self.input_label = customtkinter.CTkLabel(master=self.radiobutton_frame, text="Input", fg_color="transparent", width=30, font=('Arial', 18))
+        self.input_label = customtkinter.CTkLabel(self.radiobutton_frame, text="Input", fg_color="transparent", width=30, font=('Arial', 18, 'bold'))
         self.input_label.grid(row=0, columnspan=2)
 
         self.input_type = customtkinter.StringVar()
@@ -33,6 +33,11 @@ class main(customtkinter.CTk):
         self.url_input = customtkinter.CTkRadioButton(self.radiobutton_frame, text="URL", variable=self.input_type, value="url", height=30, width=40, )
         self.url_input.grid(row=1, column=1, pady=2)
 
+        #input textbox
+        self.input_textbox = customtkinter.CTkTextbox(self, font=('Arial', 12))
+        self.input_textbox.grid(row=1, columnspan=3, sticky="NSEW")
+        self.input = self.input_textbox.get("0.0", "end")
+
         #customization and conversion menu
         self.customization_frame = customtkinter.CTkFrame(self, corner_radius=5)
         self.customization_frame.grid(row=0, column=1, pady=5, sticky='N', ipadx=20, ipady=5)
@@ -40,19 +45,18 @@ class main(customtkinter.CTk):
         self.customization_frame.grid_columnconfigure((0,1,2), weight=1)
         self.customization_frame.grid_rowconfigure((0,1), weight=1)
 
-        self.processing_label=customtkinter.CTkLabel(master=self.customization_frame, text="Processing", fg_color="transparent", font=('Arial', 18))
+        self.processing_label=customtkinter.CTkLabel(self.customization_frame, text="Processing", fg_color="transparent", font=('Arial', 18, 'bold'))
         self.processing_label.grid(row=0, columnspan=3)
-        
-        #TODO: figure out how to only allow the entry of 2 numbers and display hint text
-        self.fontsize = customtkinter.IntVar()
-        self.fontin = customtkinter.CTkEntry(self.customization_frame, width=40, textvariable=self.fontsize, placeholder_text="12pt.", insertwidth=2)
+
+        #TODO: validate input
+        self.fontin = customtkinter.CTkEntry(self.customization_frame, width=40, placeholder_text="12pt.")
         self.fontin.grid(row=1, column=0, padx=5, pady=2.5)
 
-        self.bldpercentage = customtkinter.StringVar()
-        self.bldin = customtkinter.CTkEntry(self.customization_frame, width=40, textvariable=self.bldpercentage, placeholder_text="25%", placeholder_text_color="White")
+        self.bldin = customtkinter.CTkEntry(self.customization_frame, width=40, placeholder_text="25%")
         self.bldin.grid(row=1, column=1, padx=5, pady=2.5)
+        self.bld = self.bldin.get()
 
-        self.process_button = customtkinter.CTkButton(master=self.customization_frame, text="Submit", width=100, height=30)
+        self.process_button = customtkinter.CTkButton(self.customization_frame, text="Submit", width=100, height=30, command=self.process_text(self.input, self.bld))
         self.process_button.grid(row=1, column=2, padx=5, pady=2.5)
 
         #export menu
@@ -62,16 +66,41 @@ class main(customtkinter.CTk):
         self.export_frame.grid_columnconfigure((0,1), weight=1)
         self.export_frame.grid_rowconfigure((0,1), weight=1)
 
-        self.export_label = customtkinter.CTkLabel(master=self.export_frame, text="Export", fg_color="transparent", font=('Arial', 18))
+        self.export_label = customtkinter.CTkLabel(self.export_frame, text="Export", fg_color="transparent", font=('Arial', 18, 'bold'))
         self.export_label.grid(row=0, columnspan=2)
 
-        self.pdf_button = customtkinter.CTkButton(master=self.export_frame, text="PDF", width=75, height=30)
+        self.pdf_button = customtkinter.CTkButton(self.export_frame, text="PDF", width=75, height=30)
         self.pdf_button.grid(row=1, column=0, padx=5, pady=2.5)
 
-        self.docx_button = customtkinter.CTkButton(master = self.export_frame, text="DOCX", width=75, height=30)
+        self.docx_button = customtkinter.CTkButton(self.export_frame, text="DOCX", width=75, height=30)
         self.docx_button.grid(row=1, column=1, padx=5, pady=2.5)
 
         
+
+    #TODO: fix writing to file
+    def process_text(self, input: str, bldp: str):
+        if(bldp==''):
+            self.int_bldp=25
+        else:
+             self.int_bldp = int(bldp)
+       
+        #opens new file to write formatted input text
+        self.taggedfile = open("tagged_text.txt", "w+")
+
+        #splits text into array of words
+        self.split_text = input.split()
+
+        #loops through array to format text with bold tags
+        for word in self.split_text:
+            eindex=0
+            eindex = round(word.len()*(self.int_bldp/100))   #calculates the section to bold
+
+            self.taggedfile.write("<b>"+word[:eindex]+"<b>"+word[eindex:]+" ")  #formats text with corresponding tags
+
+        #closes txt file
+        self.taggedfile.close()
+
+
 
 if __name__ == "__main__":
     app = main()
