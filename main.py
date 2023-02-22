4f7268fd6c3df21a843b5dd571cf5f5cf8ac4104
pclark <124329926+pclark-dev@users.noreply.github.com>
2023-02-21 21:35:45 -0600

    Add web-scraping capabilities

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-import tkinter as tk
-import customtkinter
 import math
-import nltk
-import urllib
-import bs4 as bs
-import pandas as pd
 import re
+import tkinter as tk
+import urllib.request
+
+import bs4 as bs
+import customtkinter
+import nltk
+import pandas as pd
 from nltk.corpus import stopwords
+
+nltk.download('punkt')
 nltk.download('stopwords')
 
 class main(customtkinter.CTk):
@@ -36,6 +39,7 @@ class main(customtkinter.CTk):
         self.input_label.grid(row=0, columnspan=2)
 
         self.input_type = customtkinter.StringVar()
+        self.input_type.set("text")
         self.text_input =customtkinter.CTkRadioButton(self.radiobutton_frame, text="Text", variable=self.input_type, value="text", height=30, width=30)
         self.text_input.grid(row=1, column=0, pady=2)
 
@@ -67,7 +71,7 @@ class main(customtkinter.CTk):
         self.bldin.grid(row=1, column=1, padx=5, pady=2.5)
         self.bld = self.bldin.get()
 
-        self.process_button = customtkinter.CTkButton(self.customization_frame, text="Submit", width=100, height=30, command=self.process_text)
+        self.process_button = customtkinter.CTkButton(self.customization_frame, text="Submit", width=100, height=30, command=self.process_input_type)
         self.process_button.grid(row=1, column=2, padx=5, pady=2.5)
 
         #export menu
@@ -85,6 +89,13 @@ class main(customtkinter.CTk):
 
         self.docx_button = customtkinter.CTkButton(self.export_frame, text="DOCX", width=75, height=30)
         self.docx_button.grid(row=1, column=1, padx=5, pady=2.5)
+
+
+    def process_input_type(self):
+        if self.input_type.get() == "text":
+            self.process_text()
+        elif self.input_type.get() == "url":
+            self.process_url()
 
 
     def process_text(self):
@@ -140,6 +151,29 @@ class main(customtkinter.CTk):
 
         self.input_textbox.configure(font=('Arial', self.int_fontsize))
         
+    def process_url(self):
+        #read in url and parse text
+        with urllib.request.urlopen(self.input_textbox.get("1.0", 'end-1c')) as url:
+            source = url.read()
+        soup = bs.BeautifulSoup(source, 'lxml')
+
+        #remove all script and style elements
+        text = ""
+        for paragraph in soup.find_all('p'):
+            text += paragraph.text
+
+        #split text into sentences
+        sentences = nltk.sent_tokenize(text)
+
+        #clear textbox
+        self.input_textbox.delete("1.0", "end")
+        
+        #insert sentences into textbox
+        for sentence in sentences:
+            self.input_textbox.insert('end', sentence)
+
+        #process text
+        self.process_text()
 
 if __name__ == "__main__":
     app = main()
